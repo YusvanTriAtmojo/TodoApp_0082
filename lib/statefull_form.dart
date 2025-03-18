5053980b3647038432921607b24d0603dedd64bd
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-03-18 21:20:39 +0700

    Membuat file statefull

---
+++ lib/statefull_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StatefullForm extends StatefulWidget {
+  const StatefullForm({super.key});
+
+  @override
+  State<StatefullForm> createState() => _StatefullFormState();
+}
+
+class _StatefullFormState extends State<StatefullForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
