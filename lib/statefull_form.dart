6e5dc7685f1b5fd5e7e45633de8a5371ae1f0d37
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-03-19 20:27:04 +0700

    Membuat fungsi addData task, status, dateTime

--- lib/statefull_form.dart
+++ lib/statefull_form.dart
@@ -14,6 +14,31 @@ class _StatefullFormState extends State<StatefullForm> {
   DateTime? pilihDateTime;
   bool dateTime = false;
 
+  void addData() {
+    setState(() {
+      if (pilihDateTime == null) {
+        // Menampilkan alert
+        dateTime = true; 
+        return;
+      } else {
+        // Menghilangkan alert
+        dateTime = false; 
+      }
+
+      daftarTask.add({
+        'task': taskController.text,
+        'status': false,
+        'dateTime': '${pilihDateTime!.day.toString().padLeft(2, '0')}-'
+            '${pilihDateTime!.month.toString().padLeft(2, '0')}-'
+            '${pilihDateTime!.year} '
+            '${pilihDateTime!.hour.toString().padLeft(2, '0')}:' 
+            '${pilihDateTime!.minute.toString().padLeft(2, '0')}',
+      });
+      taskController.clear();
+      pilihDateTime = null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
